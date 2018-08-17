6ceea66905ddd81ee0ec9dc6239f5b5230a7fcf7
anhlhd00587 <anhlhd00587@fpt.edu.vn>
2018-08-17 13:29:38 +0700

    finish-view-404

---
+++ resources/views/error/404.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Trang 404</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Smart Error Page Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- Meta tag Keywords -->
+
+    <!-- css files -->
+    <link href="{{asset('css/404.css')}}" rel="stylesheet" type="text/css" media="all">
+    <!-- //css files -->
+
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Ropa+Sans:400,400i&amp;subset=latin-ext" rel="stylesheet">
+    <!--//online-fonts -->
+
+</head>
+<body>
+<div class="header">
+    <h1>error page</h1>
+</div>
+<div class="w3-main">
+    <div class="agile-info">
+        <h3>SORRY</h3>
+        <h2>4<img src="images/error/confused.gif" alt="image">4</h2>
+        <p>An error Occurred in the Application And Your Page could not be Served.</p>
+
+        <a href="admin/category">go back</a>
+    </div>
+
+</div>
+<div class="footer-w3l">
+    <p>&copy; 2018 Error Page. All rights reserved | Design by <a href="admin/category">Sea Fashion</a></p>
+</div>
+
+</body>
+</html>
